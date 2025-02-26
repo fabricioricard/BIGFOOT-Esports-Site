528b2f3b7f24a913fcd7b8d71bc45cbff8ed62d4
fabricioricard <fabricioricard23@gmail.com>
2025-02-26 13:59:06 -0300

    Update team.js

--- pages/team.js
+++ pages/team.js
@@ -1,7 +1,14 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import PlayerCard from '../components/PlayerCard';
 
 export default function Team() {
+  const players = [
+    { name: 'Jogador 1', role: 'Atacante', image: '/player1.jpg' },
+    { name: 'Jogador 2', role: 'Suporte', image: '/player2.jpg' },
+    { name: 'Jogador 3', role: 'Defensor', image: '/player3.jpg' },
+  ];
+
   return (
     <div>
       <Head>
@@ -10,11 +17,11 @@ export default function Team() {
       <Header />
       <main>
         <h1>Nosso Time</h1>
-        <ul>
-          <li>Jogador 1 - Posição</li>
-          <li>Jogador 2 - Posição</li>
-          <li>Jogador 3 - Posição</li>
-        </ul>
+        <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+          {players.map((player, index) => (
+            <PlayerCard key={index} name={player.name} role={player.role} image={player.image} />
+          ))}
+        </div>
       </main>
     </div>
   );
